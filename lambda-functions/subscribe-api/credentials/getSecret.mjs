9e348f0bda0fd12aeb3bb9a867656936c33973bb
Eze Ahunanya <ezeahunanya@outlook.com>
2025-02-03 19:58:58 +0000

    Modularise getSecrets function and make reusable

---
+++ lambda-functions/subscribe-api/credentials/getSecret.mjs
@@ -0,0 +1,35 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+// Cache for both DB and SMTP secrets
+let cachedSecrets = {};
+
+export const getSecret = async (secretName) => {
+  if (cachedSecrets[secretName]) {
+    console.log(`Returning cached secret for ${secretName}`);
+    return cachedSecrets[secretName];
+  }
+
+  const secretsClient = new SecretsManagerClient({
+    region: process.env.AWS_REGION,
+  });
+
+  const command = new GetSecretValueCommand({
+    SecretId: secretName,
+  });
+
+  try {
+    const secret = await secretsClient.send(command);
+    const parsedSecret = JSON.parse(secret.SecretString);
+
+    // Cache the secret
+    cachedSecrets[secretName] = parsedSecret;
+
+    return parsedSecret;
+  } catch (error) {
+    console.error(`Failed to retrieve secret for ${secretName}:`, error);
+    throw new Error(`Failed to retrieve secret: ${secretName}`);
+  }
+};
--- lambda-functions/subscribe-api/db/db.mjs
+++ lambda-functions/subscribe-api/db/db.mjs
@@ -1,30 +1,11 @@
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from "@aws-sdk/client-secrets-manager";
+import { getSecret } from "../credentials/getSecret.mjs";
 import pg from "pg";
 
 const { Client } = pg;
-let cachedDbCredentials = null;
 
 export const getDbCredentials = async () => {
-  if (cachedDbCredentials) return cachedDbCredentials;
-
-  const secretsClient = new SecretsManagerClient({
-    region: process.env.AWS_REGION,
-  });
-  const command = new GetSecretValueCommand({
-    SecretId: process.env.DB_SECRET_ARN,
-  });
-
-  try {
-    const secret = await secretsClient.send(command);
-    cachedDbCredentials = JSON.parse(secret.SecretString);
-    return cachedDbCredentials;
-  } catch (error) {
-    console.error("Failed to retrieve credentials:", error);
-    throw new Error("Failed to retrieve database credentials");
-  }
+  const dbCredentials = await getSecret(process.env.DB_SECRET_ARN);
+  return dbCredentials;
 };
 
 export const connectToDatabase = async (dbCredentials) => {
