c5bc50855835b4ca8040ea235a84de8876557145
Eze Ahunanya <ezeahunanya@outlook.com>
2025-02-06 18:42:30 +0000

    Remove unused variable in generate token function

--- lambda/functions/subscribe-api/db/generateUniqueToken.mjs
+++ lambda/functions/subscribe-api/db/generateUniqueToken.mjs
@@ -4,16 +4,11 @@ import crypto from "crypto";
  * Generates a unique token and its hash, ensuring it doesn't already exist in the database.
  *
  * @param {Object} client - The database client.
- * @param {string} tokenTableName - The name of the token table.
  * @param {number} maxRetries - The maximum number of attempts to generate a unique token.
  * @returns {Object} - An object containing the token and its hash.
  * @throws {Error} - If a unique token cannot be generated after the maximum retries.
  */
-export const generateUniqueToken = async (
-  client,
-  tokenTableName,
-  maxRetries = 5
-) => {
+export const generateUniqueToken = async (client, maxRetries = 10) => {
   let retries = 0;
   let token, tokenHash, isUnique;
 
