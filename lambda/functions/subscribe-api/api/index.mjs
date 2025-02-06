db113f37f143a0fb3119b62347aabafd348aef6b
Eze Ahunanya <ezeahunanya@outlook.com>
2025-02-06 21:35:04 +0000

    Pass error message to frontend

--- lambda/functions/subscribe-api/api/index.mjs
+++ lambda/functions/subscribe-api/api/index.mjs
@@ -44,7 +44,7 @@ export const handler = async (event) => {
     console.error(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
+      body: JSON.stringify({ error: error.message }),
     };
   } finally {
     if (client) {
